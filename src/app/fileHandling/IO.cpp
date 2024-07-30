17685c5437576992059e7fa30486e9f251e13d06
Priyanshu24Sinha <142648200+Priyanshu24Sinha@users.noreply.github.com>
2024-07-30 08:01:44 +0000

    Task commits

---
+++ src/app/fileHandling/IO.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "IO.hpp"
+#include <fstream>
+
+IO::IO(const std::string &file_path){
+
+file_stream.open(file_path, std::ios::in | std::ios::out | std::ios::binary);
+if(!file_stream.is_open()){
+    std::cout<<"Unable to open the file Now"<<file_path<<std::endl;
+}
+
+}
+
+std::fstream IO::getFileStream(){
+    return std::move(file_stream);
+}
